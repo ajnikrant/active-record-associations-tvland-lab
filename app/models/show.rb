e7f99ba8437a6fea6c9a20773484fa3adfac89a1
Amanda <ajnikrant@gmail.com>
2021-03-05 08:36:36 -0500

    commit tree cec2e5dc6916ba72dc08f8248be9147ec36efd97

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :character
+  belongs_to :network
+
+  def actors_list
+    self.characters.collect{|character| character.actor.full_name}
+
+    # binding.pry
+    end
 end
